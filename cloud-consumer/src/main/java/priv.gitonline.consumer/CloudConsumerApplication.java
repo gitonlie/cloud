9a2580150ddc7edd9612d0c5ce4e1bc373fd5b89
duke <1181249332@qq.com>
2020-01-10 18:07:03 +0800

    提交

--- cloud-consumer/src/main/java/priv.gitonline.consumer/CloudConsumerApplication.java
+++ cloud-consumer/src/main/java/priv.gitonline.consumer/CloudConsumerApplication.java
@@ -6,11 +6,21 @@ import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @SpringCloudApplication
+ * 相当于
+ * @EnableHystrix
+ * @EnableEurekaClient
+ * @SpringBootApplication
+ */
+@EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
 public class CloudConsumerApplication {
--- cloud-consumer/src/main/java/priv.gitonline.consumer/controller/OrderController.java
+++ cloud-consumer/src/main/java/priv.gitonline.consumer/controller/OrderController.java
@@ -1,5 +1,6 @@
 package priv.gitonline.consumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,19 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/addOrder")
+    @HystrixCommand(fallbackMethod = "failed")
     public Map<String,Object> addOrder(String data){
         Map<String,Object> params = new HashMap<>();
         params.put("data",data);
         Map<String,Object> result = restTemplate.postForObject("http://cloud-provider/addOrder?data={data}",null,Map.class,params);
+        return result;
+    }
+
+    public Map<String,Object> failed(String data){
+        Map<String,Object> result = new HashMap<>();
+        result.put("code","9999");
+        result.put("msg","调用失败");
+        result.put("data",data);
         return result;
     }
 
